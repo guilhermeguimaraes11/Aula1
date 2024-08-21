359baa79dc487e37a23d7f2a648d98a5d986964a
GuilhermeG <guilherme10007@gmail.com>
2024-08-21 11:18:39 -0300

    21-08

---
+++ src/index.js
@@ -0,0 +1,33 @@
+ //Importa o módulo Express
+ const express = require('express')
+
+ //Define uma classe para organizar a lógica da aplicação
+ class AppController{
+ 
+     constructor(){
+         //Cria uma nova instância do Express dentro da classe
+         this.express = express();
+ 
+         //Chama o método middlewares para configurar os middlewares
+         this.middlewares();
+ 
+         //Chama o método routes para definir as rotas da Api
+         this.routes();
+     }
+     middlewares(){
+         //Permitir que a aplicação receba dados em formato JSON nas requisições
+         this.express.use(express.json());
+     }
+     //Define as rotas da nossa API
+     routes() {
+         this.express.get("/health/", (req,res)=>{
+             res.send({Nome:"Guilherme",
+                    Idade:"17",
+                    CPF:"45558053801"
+             });
+         }); //Essa rota é usada para verificar se Api está OK
+     }    
+ }
+ //Exportando a instância de Express configurada, para que seja acessada em outros arquivos
+ module.exports = new AppController().express;
+ 
