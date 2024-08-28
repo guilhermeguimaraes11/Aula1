40c897fb1530f81e8e2722af8da876a1e114f1c2
Guilhermeg <Guilherme10007@gmail.com>
2024-08-28 14:10:02 -0300

    atividadeAPI21-08

--- 21-08/src/index.js
+++ 21-08/src/index.js
@@ -1,33 +1,61 @@
- //Importa o módulo Express
- const express = require('express')
+const express = require("express");
 
- //Define uma classe para organizar a lógica da aplicação
- class AppController{
- 
-     constructor(){
-         //Cria uma nova instância do Express dentro da classe
-         this.express = express();
- 
-         //Chama o método middlewares para configurar os middlewares
-         this.middlewares();
- 
-         //Chama o método routes para definir as rotas da Api
-         this.routes();
-     }
-     middlewares(){
-         //Permitir que a aplicação receba dados em formato JSON nas requisições
-         this.express.use(express.json());
-     }
-     //Define as rotas da nossa API
-     routes() {
-         this.express.get("/health/", (req,res)=>{
-             res.send({Nome:"Guilherme",
-                    Idade:"17",
-                    CPF:"45558053801"
-             });
-         }); //Essa rota é usada para verificar se Api está OK
-     }    
- }
- //Exportando a instância de Express configurada, para que seja acessada em outros arquivos
- module.exports = new AppController().express;
- 
+class AppController {
+  constructor() {
+    this.express = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.express.use(express.json());
+  }
+
+  routes() {
+    const users = [];
+
+    this.express.post("/users", (req, res) => {
+      const { id, nome, email, senha } = req.body;
+      users.push({ id, nome, email, senha });
+      res.status(200).send({ message: "Usuário cadastrado com sucesso" });
+    });
+
+    this.express.post("/auth", (req, res) => {
+      const { email, senha } = req.body;
+
+      const user = users.find((user) => user.email === email);
+
+      if (user) {
+        const user = users.find((user) => user.senha === senha);
+        if (user) {
+          return res.status(200).send({ message: "Usuario Autenticado" });
+        } else {
+          return res.status(400).send({ message: "Senha Incorreta" });
+        }
+      } else {
+        return res.status(400).send({ message: "Email Incorreto" });
+      }
+    });
+
+    this.express.get("/users/:id", (req, res) => {
+      const { id } = req.params;
+      const user = users.find((user) => user.id == id);
+      if (user) {
+        res.status(200).send(user);
+      } else {
+        res.status(400).send({ message: "Usuário não encontrado" });
+      }
+    });
+
+    this.express.get("/health/", (req, res) => {
+      res.send({
+        Nome: "Guilherme",
+        Idade: "17",
+        CPF: "45558053801",
+        Email: "guilherme10007@gm",
+      });
+    });
+  }
+}
+
+module.exports = new AppController().express;
--- 21-08/src/server.js
+++ 21-08/src/server.js
@@ -1,4 +1,4 @@
 // Importar a instância do Express configurada em index.js
 const app = require("./index");
-//Inicia o servidor na porta 3000, neste caso a API será acessivel em http://localhost
-app.listen(3000);
+//Inicia o servidor na porta 5000, neste caso a API será acessivel em http://localhost:5000
+app.listen(5000);
