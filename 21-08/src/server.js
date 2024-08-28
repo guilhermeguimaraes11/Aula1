6df221ea99cfa558e888e43fc48a958da0334feb
Guilhermeg <Guilherme10007@gmail.com>
2024-08-28 14:23:13 -0300

    atividadeAPI21-08

--- 21-08/src/server.js
+++ 21-08/src/server.js
@@ -1,4 +1,4 @@
 // Importar a instância do Express configurada em index.js
-const app = require("./index");
+const app = require("./index21-08");
 //Inicia o servidor na porta 5000, neste caso a API será acessivel em http://localhost:5000
 app.listen(5000);
